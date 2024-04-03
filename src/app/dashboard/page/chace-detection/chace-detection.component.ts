9b82556372c26a7c853a82fb88283d1aa365eeea
jose angel <jose.albelt1817@gmail.com>
2024-04-03 08:02:35 -0500

    change detection

--- src/app/dashboard/page/chace-detection/chace-detection.component.ts
+++ src/app/dashboard/page/chace-detection/chace-detection.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { TitleComponent } from "../../../shared/title/title.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-chace-detection',
-  standalone: true,
-  imports: [],
-  templateUrl: './chace-detection.component.html',
-  styleUrl: './chace-detection.component.css'
+    
+    selector: 'app-chace-detection',
+    standalone: true,
+    imports: [TitleComponent,CommonModule],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+
+
+    template: `
+    <app-title [title]="currrentFrameWork()"/>
+
+
+    <pre>{{frameWork()   | json     }}</pre>
+    <pre>{{frameWorkAsProperty | json  }}</pre>
+    `
 })
 export default class ChaceDetectionComponent {
+
+  public currrentFrameWork = computed(
+    () => `chance detection = ${this.frameWork().name}`
+  );
+
+
+  public frameWork = signal({
+    name: 'angular',
+    aye: 2016
+  })
+
+  public frameWorkAsProperty = {
+    name: 'angular',
+    aye: 2016
+  }
+
+
+  constructor(){
+    setTimeout(() =>{
+      // this.frameWorkAsProperty.name = 'React';
+
+      this.frameWork.update(value =>({
+        ...value,
+        name:'React'
+      }))
+
+      console.log('Hecho');
+    },3000);
+  }
 
 }
